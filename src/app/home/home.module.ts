d2c023be742ea036987ec74f62ed7f758ec0b6c8
prrocontento <jeee@gft.com>
2021-02-02 09:12:47 -0600

    entrega 1

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { HomePageRoutingModule } from './home-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
